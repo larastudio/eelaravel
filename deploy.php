752ecba309619d997660447d4fb473ced89b1c25
Jasper Frumau <jasper@imagewize.com>
2019-07-20 10:53:09 +0700

    new deploy test for Docker / EasyEngine

--- deploy.php
+++ deploy.php
@@ -3,6 +3,12 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
+// PHP FPM 
+
+set('php_fpm_service', 'php7.2-fpm');
+set('php_fpm_command', 'echo "" | sudo -S /usr/sbin/service {{php_fpm_service}} reload'); // for setups like laravel forge
+set('php_fpm_command', "kill -USR2 $(ps -ef | grep '[p]hp-fpm: master' | awk '{print $2}')"); // for docker based setups with laradock or similar
+
 // Configuration
 set('repository', 'git@github.com:Larastudio/lslaravel.git');
 set('default_stage', 'production');
@@ -36,17 +42,19 @@ task('horizon:terminate', function () {
    ->user('web')
    ->forwardAgent()
    ->stage('staging')
-   ->set('deploy_path', '/home/web/staging.larastud.io');
+   ->set('deploy_path', '/opt/easyengine/sites/staging.lara.studio');
 
    host('production')
    ->hostname('larastud.io')
    ->user('web')
    ->forwardAgent()
    ->stage('production')
-   ->set('deploy_path', '/home/web/laravel');
+   ->set('deploy_path', '/opt/easyengine/sites/lara.studio');
 
 
 // Run database migrations
 after('deploy:symlink', 'db:migrate');
+after('deploy', 'fpm:reload');
+
 
 
